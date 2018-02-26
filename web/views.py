69f81d72e91ce61bc52b2342704b4b8cece6b448
user <user@localhost.localdomain>
2018-02-26 03:33:59 -0500

    Make Token per User

--- web/views.py
+++ web/views.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+def submit_expense(request):
+    """user submits an Expense"""
+    print ('Salam, Khosh Amadi')
 # Create your views here.
